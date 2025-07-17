1277f9429fa51cf68c3f6f157c916ce78cbf2f14
bescobaralfaro <bescobaralfaro@cwls.us>
2025-07-17 12:39:11 -0400

    Update app.py
    changed app.run

--- app.py
+++ app.py
@@ -62,4 +62,5 @@ def list_files():
 	    return jsonify({"error": "Could not acquire token", "details": token_response})
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	port = int(os.environ.get("PORT", 10000))
+	app.run(host = "0.0.0.0", port = port)
